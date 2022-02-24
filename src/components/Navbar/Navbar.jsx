510a30a4966f8a551963c01fae1299842e8eba0a
emmanuelkamala <emmanuelkamala86@gmail.com>
2022-02-24 18:32:37 +0300

    Add a hamburger menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { MdOutlineRestaurantMenu } from 'react-icons/md';
 
@@ -6,35 +7,43 @@ import images from '../../constants/images';
 
 import './Navbar.css';
 
-const Navbar = () => (
-  <nav className="app__navbar">
-    <div className="app__navbar-logo">
-      <img src={images.logo} alt="royal logo" />
-    </div>
-    <ul className="app__navbar-links">
-      <li className="p__opensans"><a href="#home">Home</a></li>
-      <li className="p__opensans"><a href="#about">About</a></li>
-      <li className="p__opensans"><a href="#menu">Menu</a></li>
-      <li className="p__opensans"><a href="#awards">Awards</a></li>
-      <li className="p__opensans"><a href="#contact">Contact</a></li>
-    </ul>
-    <div className="app__navbar-booking">
-      <a href="/" className="p__opensans">Book Table</a>
-    </div>
-    <div className="app__navbar-smallscreen">
-      <GiHamburgerMenu color="#fff" fontSize={27} onClick={() => {}} />
-      <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
-        <MdOutlineRestaurantMenu fontSize={27} className="overlay__close" onClick={() => {}} />
-        <ul className="app__navbar-smallscreen-links">
-          <li className="p__opensans"><a href="#home">Home</a></li>
-          <li className="p__opensans"><a href="#about">About</a></li>
-          <li className="p__opensans"><a href="#menu">Menu</a></li>
-          <li className="p__opensans"><a href="#awards">Awards</a></li>
-          <li className="p__opensans"><a href="#contact">Contact</a></li>
-        </ul>
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+  return (
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <img src={images.logo} alt="royal logo" />
       </div>
-    </div>
-  </nav>
-);
+      <ul className="app__navbar-links">
+        <li className="p__opensans"><a href="#home">Home</a></li>
+        <li className="p__opensans"><a href="#about">About</a></li>
+        <li className="p__opensans"><a href="#menu">Menu</a></li>
+        <li className="p__opensans"><a href="#awards">Awards</a></li>
+        <li className="p__opensans"><a href="#contact">Contact</a></li>
+      </ul>
+      <div className="app__navbar-booking">
+        <a href="/" className="p__opensans">Book Table</a>
+      </div>
+      <div className="app__navbar-smallscreen">
+        <GiHamburgerMenu color="#fff" fontSize={27} onClick={() => setToggleMenu(true)} />
+        {
+          toggleMenu && (
+            <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
+              <MdOutlineRestaurantMenu fontSize={27} className="overlay__close" onClick={() => setToggleMenu(false)} />
+              <ul className="app__navbar-smallscreen-links">
+                <li className="p__opensans"><a href="#home">Home</a></li>
+                <li className="p__opensans"><a href="#about">About</a></li>
+                <li className="p__opensans"><a href="#menu">Menu</a></li>
+                <li className="p__opensans"><a href="#awards">Awards</a></li>
+                <li className="p__opensans"><a href="#contact">Contact</a></li>
+              </ul>
+            </div>
+          )
+        }
+        
+      </div>
+    </nav>
+  )
+};
 
 export default Navbar;
