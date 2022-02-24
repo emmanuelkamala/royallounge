47dc6ee76e0e6fe949db8ab643a3cd8859d775a0
emmanuelkamala <emmanuelkamala86@gmail.com>
2022-02-24 19:21:42 +0300

    Edit header component

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -8,10 +8,12 @@ const Header = () => (
     <div className="app__wrapper_info">
       <SubHeading title="Chase the New Flavour" />
       <h1 className="app__header-h1">The Key to Fine Dining</h1>
+      <p className="p__opensans" style={{ margin: '2rem 0' }}>Welcome to Royal Lounge Menu</p>
+      <button type="button" className="custom__button">Explore Menu</button>
     </div>
 
     <div className="app__wrapper_img">
-
+      <img src={images.welcome} alt="welcome" />
     </div>
   </div>
 );
