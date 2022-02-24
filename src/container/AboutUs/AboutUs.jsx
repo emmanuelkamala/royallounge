1cfd5f2f1588ce767d1ef21933906b50d0d711fe
emmanuelkamala <emmanuelkamala86@gmail.com>
2022-02-24 21:28:09 +0300

    Create About us component

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
-
+import { images } from '../../constants';
 import './AboutUs.css';
 
 const AboutUs = () => (
-  <div>
-    AboutUs
+  <div className="app__aboutus app__bg flex__center section__padding" id="about">
+    <div className="app__aboutus-overlay flex__center">
+      <img src={images.R} alt="symbol" className="app__aboutus-r" />
+    </div>
+
+    <div className="app__aboutus-content flex__center">
+      <div className="app__aboutus-content_about">
+        <h1 className="headtext__cormorant">About Us</h1>
+        <img src={images.spoon} alt="about spoon" className="spoon__img" />
+        <p className="p__opensans">Royal lounge is a new food service in Sheffield</p>
+        <button type="button" className="custom__button">Read More</button>
+      </div>
+
+      <div className="app__aboutus-content_knife flex__center">
+        <img src={images.knife} alt="about spoon" className="knife__img" />
+      </div>
+
+      <div className="app__aboutus-content_history">
+        <h1 className="headtext__cormorant">Our History</h1>
+        <img src={images.spoon} alt="about spoon" className="spoon__img" />
+        <p className="p__opensans">Royal lounge does go a long way back</p>
+        <button type="button" className="custom__button">Read More</button>
+      </div>
+    </div>
   </div>
 );
 
