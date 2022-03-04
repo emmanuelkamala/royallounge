49a0e8b67fdc6c1230257b7e205f911e7a07a7a4
emmanuelkamala <emmanuelkamala86@gmail.com>
2022-03-04 10:27:07 +0300

    Add menu items

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -12,7 +12,9 @@ const AboutUs = () => (
       <div className="app__aboutus-content_about">
         <h1 className="headtext__cormorant">About Us</h1>
         <img src={images.spoon} alt="about spoon" className="spoon__img" />
-        <p className="p__opensans">Royal lounge is a new food service in Sheffield</p>
+        <p className="p__opensans">
+          Welcome to our venue Royal Lounge, we are located at St Mary`s road in the heart of Sheffield the venue is not very far from Sheffield united Stadium. 
+          We are offering all sorts of drinks, from soft drinks to alcohol and cocktails. We are also Bringing the best Lebanese cuisine in Sheffield.</p>
         <button type="button" className="custom__button">Read More</button>
       </div>
 
@@ -21,9 +23,13 @@ const AboutUs = () => (
       </div>
 
       <div className="app__aboutus-content_history">
-        <h1 className="headtext__cormorant">Our History</h1>
+        <h1 className="headtext__cormorant">More...</h1>
         <img src={images.spoon} alt="about spoon" className="spoon__img" />
-        <p className="p__opensans">Royal lounge does go a long way back</p>
+        <p className="p__opensans">
+          Visit us to try our Cold starters like Hummus or Hot starters like Sambusak or try our Royal Vegan Platter or Royal Mezze Meat . 
+          We also have plenty of Parking space for our customers and outside seating area for customers who prefer to sit outside. 
+          We also cater for Corporate events, Birthday Parties and Weddings. 
+        </p>
         <button type="button" className="custom__button">Read More</button>
       </div>
     </div>
