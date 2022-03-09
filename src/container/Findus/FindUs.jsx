e8f18b855fe2842edbb4dec1bf89b0020acdc8a5
emmanuelkamala <emmanuelkamala86@gmail.com>
2022-03-10 01:43:18 +0300

    Create a find us container

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
 
 const FindUs = () => (
-  <div>
-    FindUs
+  <div className="app__bg app__wrapper section__padding" id="contact">
+    <div className="app__wrapper_info">
+      <SubHeading title="Contact" />
+      <h1 className="headtext__cormorant" style={{ marginBottom: '3rem'}}>
+        Find Us
+      </h1>
+      <div className="app__wrapper-content">
+        <p className="p__opensans">Phone: 0114 201 9675</p>
+        <p className="p__cormorant" style={{ color: '#DCCA87', margin: '2rem 0'}}>Opening Hours</p>
+        <p className="p__opensans">Mon - Fri: </p>
+        <p className="p__opensans">Sat - Sun: </p>
+      </div>
+    </div>
+    <div className="app__wrapper_img">
+      <img src={images.findus} alt="find us" />
+    </div>
   </div>
 );
 
