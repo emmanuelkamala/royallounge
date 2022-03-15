75570b04b5cd49654bce839597783c1c9f041226
Emmanuel Kamala <emmanuelkamala86@gmail.com>
2022-03-16 00:40:27 +0300

    Edit footer

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -6,7 +6,7 @@ const Newsletter = () => (
   <div className="app__newsletter">
     <div className="app__newsletter-heading">
       <SubHeading title="Come and Enjoy" /> 
-      <p className="headtext__cormorant">"You don’t need to have a Rolls-Royce kitchen to make a great meal."</p> 
+      <p className="headtext__cormorant" style={{ textAlign: 'center'}}>"You don’t need to have a Rolls-Royce kitchen to serve a great meal."</p> 
     </div>
   </div>
 );
--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -6,7 +6,7 @@ import './Footer.css';
 
 const Footer = () => (
   <div className="app__footer section__padding">
-    <FooterOverlay />
+    {/* <FooterOverlay /> */}
     <Newsletter />
 
     <div className="app__footer-links">
